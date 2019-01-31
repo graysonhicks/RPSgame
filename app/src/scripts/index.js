ba10a29967830e61aa8ede544703b5bfb91f2faa
Carlos Flores <carlosfloresalex@gmail.com>
2019-01-31 00:46:39 -0500

    added javascript, made the buttons for the humanInput and gave back a value

--- app/src/scripts/index.js
+++ app/src/scripts/index.js
@@ -5,4 +5,32 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles/index.scss"
 
-console.log("This is barlos speaking")
+//declaring vairiables
+var humanInput = '0';
+var computerInput = '0';
+
+//checks the human input 
+var rockImage = document.getElementById("rock");
+var paperImage = document.getElementById("paper");
+var scissorImage = document.getElementById("scissor");
+
+rockImage.onclick = function (e) {
+    console.log("rock");
+    humanInput = '0';
+    console.log(humanInput);
+}
+paperImage.onclick = function (e) {
+    console.log("paper");
+    humanInput = '1';
+    console.log(humanInput);
+}
+scissorImage.onclick = function (e) {
+    console.log("scissor");
+    humanInput = '2';
+    console.log(humanInput);
+}
+var result = humanInput + computerInput;
+
+//make a random generator for the computer for its input
+computerInput = Math.floor(Math.random() * 3);
+console.log(computerInput);
